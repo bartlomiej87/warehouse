8dd9699a8a9868b5c1550e3f31a210e7e8633b6b
Bartlomiej Baba <bbaba@pzu.pl>
2021-12-17 14:07:34 +0100

    tests

--- src/main/kotlin/com/challenge/warehouse/WarehouseController.kt
+++ src/main/kotlin/com/challenge/warehouse/WarehouseController.kt
@@ -12,6 +12,7 @@ class WarehouseController(private val patientsRepository: PatientRepository) {
 
     @GetMapping("ware")
     fun getAllPatients(): ResponseEntity<MutableList<Patient>> {
+
         patientsRepository.save(Patient(name = "test", description = "test"))
         return ResponseEntity.ok(patientsRepository.findAll())
     }
