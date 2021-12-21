8323d5b4cdbc7a2894781b808368d9f7e7b5ab52
Bartlomiej Baba <bbaba@pzu.pl>
2021-12-21 18:00:17 +0100

    Added ITs cases

--- src/test/kotlin/com/challenge/warehouse/integration/analytics/AnalyticsAggregatorQueryTests.kt
+++ src/test/kotlin/com/challenge/warehouse/integration/analytics/AnalyticsAggregatorQueryTests.kt
@@ -3,6 +3,8 @@ package com.challenge.warehouse.integration.analytics
 import com.challenge.warehouse.integration.BaseIT
 import com.challenge.warehouse.integration.IntegrationTest
 import com.challenge.warehouse.model.Metric
+import com.challenge.warehouse.model.Metric.CLICKS
+import com.challenge.warehouse.model.Metric.IMPRESSIONS
 import com.challenge.warehouse.repository.AnalyticsAggregatorRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.math.BigDecimal
 import java.math.RoundingMode.HALF_UP
+import java.time.LocalDate
 
 @IntegrationTest
 class AnalyticsAggregatorQueryTests : BaseIT() {
@@ -39,6 +42,115 @@ class AnalyticsAggregatorQueryTests : BaseIT() {
             assertEquals(1022, twitterAnalytics.totalClicks)
             assertEquals(8244, twitterAnalytics.totalImpressions)
             assertEquals(BigDecimal("0.12"), twitterAnalytics.clickThroughRate?.setScale(2, HALF_UP))
+        }
+    }
+
+    @Test
+    fun `should aggregate metrics - group by datasource and filter date from and date to`() {
+        //when
+        val result = aggregatorRepository.aggregateByDatasource(
+            metrics,
+            dateFrom = LocalDate.parse("2019-11-12"),
+            dateTo = LocalDate.parse("2019-11-12")
+        )
+
+        // then
+        with(result) {
+            assertEquals(2, size)
+            val googleAnalytics = single { it.dimensionName == googleId }
+            val facebookAnalytics = single { it.dimensionName == facebookId }
+            assertEquals(7, googleAnalytics.totalClicks)
+            assertEquals(22425, googleAnalytics.totalImpressions)
+            assertEquals(BigDecimal("0.00"), googleAnalytics.clickThroughRate?.setScale(2, HALF_UP))
+            assertEquals(79, facebookAnalytics.totalClicks)
+            assertEquals(40887, facebookAnalytics.totalImpressions)
+            assertEquals(BigDecimal("0.00"), facebookAnalytics.clickThroughRate?.setScale(2, HALF_UP))
+        }
+    }
+
+    @Test
+    fun `should aggregate metrics - group by datasource and filter date from`() {
+        //when
+        val result = aggregatorRepository.aggregateByDatasource(
+            metrics,
+            dateFrom = LocalDate.parse("2019-11-12"), null
+        )
+
+        // then
+        with(result) {
+            assertEquals(2, size)
+            val googleAnalytics = single { it.dimensionName == googleId }
+            val facebookAnalytics = single { it.dimensionName == facebookId }
+            assertEquals(23, googleAnalytics.totalClicks)
+            assertEquals(67877, googleAnalytics.totalImpressions)
+            assertEquals(BigDecimal("0.00"), googleAnalytics.clickThroughRate?.setScale(2, HALF_UP))
+            assertEquals(79, facebookAnalytics.totalClicks)
+            assertEquals(40887, facebookAnalytics.totalImpressions)
+            assertEquals(BigDecimal("0.00"), facebookAnalytics.clickThroughRate?.setScale(2, HALF_UP))
+        }
+    }
+
+    @Test
+    fun `should aggregate metrics - group by datasource and filter date to`() {
+        //when
+        val result = aggregatorRepository.aggregateByDatasource(
+            metrics,
+            dateTo = LocalDate.parse("2019-06-05"), dateFrom = null
+        )
+
+        // then
+        with(result) {
+            assertEquals(1, size)
+            val googleAnalytics = single { it.dimensionName == googleId }
+            assertEquals(1, googleAnalytics.totalClicks)
+            assertEquals(261, googleAnalytics.totalImpressions)
+            assertEquals(BigDecimal("0.00"), googleAnalytics.clickThroughRate?.setScale(2, HALF_UP))
+        }
+    }
+
+    @Test
+    fun `should aggregate clicks by datasource and filter by date from and date to`() {
+        //when
+        val result = aggregatorRepository.aggregateByDatasource(
+            setOf(CLICKS),
+            dateFrom = LocalDate.parse("2019-11-12"),
+            dateTo = LocalDate.parse("2019-11-12")
+        )
+
+        // then
+        with(result) {
+            assertEquals(2, size)
+            val googleAnalytics = single { it.dimensionName == googleId }
+            val facebookAnalytics = single { it.dimensionName == facebookId }
+            assertEquals(7, googleAnalytics.totalClicks)
+            assertNull(googleAnalytics.totalImpressions)
+            assertNull(googleAnalytics.clickThroughRate)
+            assertEquals(79, facebookAnalytics.totalClicks)
+            assertNull(facebookAnalytics.totalImpressions)
+            assertNull(facebookAnalytics.clickThroughRate)
+        }
+    }
+
+    @Test
+    fun `should aggregate impressions by datasource and filter by date from and date to`() {
+        //when
+        val result = aggregatorRepository.aggregateByDatasource(
+            setOf(IMPRESSIONS),
+            dateFrom = LocalDate.parse("2019-11-12"),
+            dateTo = LocalDate.parse("2019-11-12")
+        )
+
+        // then
+        with(result) {
+            assertEquals(2, size)
+            val googleAnalytics = single { it.dimensionName == googleId }
+            val facebookAnalytics = single { it.dimensionName == facebookId }
+            assertEquals(22425, googleAnalytics.totalImpressions)
+            assertNull(googleAnalytics.totalClicks)
+            assertNull(googleAnalytics.clickThroughRate)
+            assertEquals(40887, facebookAnalytics.totalImpressions)
+            assertNull(facebookAnalytics.totalClicks)
+            assertNull(facebookAnalytics.clickThroughRate)
         }
     }
 
